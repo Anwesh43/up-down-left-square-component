bc617de88841355e53c7af5f722b1646d1c867ff
Anwesh43 <anweshthecool0@gmail.com>
2020-04-24 02:47:01 +0530

    adding UpDownLeftSquares component

---
+++ src/UpDownLeftSquares.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useAnimatedScale, useDimension} from './hooks'
+import UpDownLeftSquare from './UpDownLeftSquare'
+
+const btnStyle = (w, h) => {
+    return {position : 'absolute', left : `${w / 2}px`, top : `${0.1 * h}px`}
+}
+
+const UpDownLeftSquares = (props) => {
+    const {w, h, resize, enableResize} = useDimension()
+    const {scale, start} = useAnimatedScale(0.02, 20)
+    return <div>
+        <button style = {btnStyle(w, h)} onClick = {start}>Start</button>
+        {[0, 1].map(i => <UpDownLeftSquare key = {`rect_${i}`} i = {i} w = {w} h = {h} scale = {scale}/>)}
+    </div>
+}
+
+export default UpDownLeftSquares
