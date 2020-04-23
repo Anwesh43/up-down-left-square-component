64f618ef866c4045478604f97d6c7a1419c2c6eb
Anwesh43 <anweshthecool0@gmail.com>
2020-04-24 02:14:13 +0530

    adding utill functions

---
+++ src/utils.js
@@ -0,0 +1,3 @@
+const maxScale = (scale, i, n) => Math.max(0, scale - i / n)
+export const divideScale = (scale, i, n) => Math.min(1 / n, maxScale(scale, i, n)) * n
+export const sinify = (scale) => Math.sin(scale * Math.PI)
